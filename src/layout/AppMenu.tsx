88c8db8170cf1fc9b39bf0a027c1dcb850f66917
GinoGallardo <ginosgt@gmail.com>
2023-09-06 20:14:10 -0300

    listado de modulos con json server

--- src/layout/AppMenu.tsx
+++ src/layout/AppMenu.tsx
@@ -13,7 +13,10 @@ const AppMenu = () => {
     const model: AppMenuItem[] = [
         {
             label: 'Home',
-            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+            items: [
+                { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
+                { label: 'Modules', icon: 'pi pi-fw pi-th-large', to: '/modules'}
+            ]
         },
     ];
 
