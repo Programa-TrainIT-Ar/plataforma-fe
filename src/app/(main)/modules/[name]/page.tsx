6eb8dc9a820acb13ffa83704d9c846a964a4ed29
Exequiel Bobruk <bobrukfs@gmail.com>
2023-09-11 11:24:38 -0300

    feat: add module details

---
+++ src/app/(main)/modules/[name]/page.tsx
@@ -0,0 +1,59 @@
+
+'use client'
+import React, { useState, useEffect } from 'react';
+import { ProductService } from '../../../../../src/demo/service/ProductService';
+import { DataView } from 'primereact/dataview';
+import { Card } from 'primereact/card';
+import Link from 'next/link'
+import {useSearchParams} from "next/navigation";
+
+
+export default function Modules() {
+    const [products, setProducts] = useState([]);
+
+    const searchParams = useSearchParams();
+    const id = searchParams.get("id") - 1;
+    console.log(id);
+    
+    useEffect(() => {
+      ProductService.getModules().then((data) => setProducts(data));
+  }, []); 
+
+ 
+
+  const itemTemplate = (data) => {
+        return (
+          <div className="card w-full">
+ 
+              <div className='flex  align-items-center justify-content-between'>
+                  <h2>{data.name}</h2>
+                  <p>{data.edition}</p>
+              </div>
+              <p className="m-0">
+                {data.description}
+              </p>
+              <div className='flex flex-wrap gap-4 justify-content-center m-5'>
+                 {data.cells.map((cell, i) => {
+                  console.log(cell);
+                    return (
+                      <>
+                         <Card className="w-5" title={`${cell.name}`} key={i} subTitle={`${cell.lider}`} >
+             
+                          </Card>
+                      </>
+                    )
+                  })}
+             
+              </div>
+      </div>
+        );
+  }
+
+  return (
+    <div className="card">
+      <DataView value={products.slice(id, id + 1)} itemTemplate={itemTemplate} />
+    </div>
+  );
+
+}
+        
--- src/app/(main)/modules/page.tsx
+++ src/app/(main)/modules/page.tsx
@@ -4,13 +4,17 @@ import React, { useState, useEffect } from 'react';
 import { ProductService } from '../../../../src/demo/service/ProductService';
 import { DataView } from 'primereact/dataview';
 import ButtonModules from '../ButtonModules/ButtonModules';
+import Link from 'next/link'
+import { useRouter } from "next/navigation";
 
 export default function Modules() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         ProductService.getModules().then((data) => setProducts(data));
-    }, []);      
+    }, []); 
+
+
 
     const itemTemplate = (data) => {
         return (
@@ -19,7 +23,10 @@ export default function Modules() {
                     <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${data.image}`} alt={data.name} />
                     <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
                         <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-                            <div className="text-2xl font-bold text-900">{data.name}</div>
+                            <Link href={{
+                                pathname: `/modules/${data.name}`,
+                                query : data,
+                                }} className="text-2xl font-bold text-900">{data.name}</Link>
                             <div className="flex align-items-center gap-3">
                                 <span className="flex align-items-center gap-2">
                                     <span className="font-semibold">{data.description}</span>
@@ -30,6 +37,7 @@ export default function Modules() {
                         <div className="flex sm:flex-column align-items-center sm:align-items-end sm:gap-2">
                             <ButtonModules/>
                         </div>
+                       
                     </div>
                 </div>
             </div>
