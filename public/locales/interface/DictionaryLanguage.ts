98941ecfc132dccbd07ce5ba376451ee737872db
Aramis jaime <you@example.com>
2023-12-12 11:00:04 -0300

    Add words and setences to traslate

--- public/locales/interface/DictionaryLanguage.ts
+++ public/locales/interface/DictionaryLanguage.ts
@@ -2,6 +2,13 @@ export interface DictionaryLanguage {
     "HelloWorld":string
     "Counter":string
     "Name":string
+    "Create":string
+    "ObjectiveOfTheModule":string
+    "Description":string
+    "NameOfModule":string
+    "InitDate":string
+    "Search":string
+  
 }
 
 export enum LanguageAvailable {
