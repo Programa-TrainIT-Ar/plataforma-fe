23c3567fd8c4c96350d4aba00dec03839f7529a4
Aramis jaime <you@example.com>
2023-12-18 17:08:20 -0300

    funcionality implemented on page component and repository

--- src/app/(main)/module/create/page.tsx
+++ src/app/(main)/module/create/page.tsx
@@ -6,26 +6,34 @@ import DateInitial from '../components/DateInitial';
 import { InputTextarea } from 'primereact/inputtextarea';
 import SearchModule from '../components/SearchModule';
 import { Button } from 'primereact/button';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { useCasesHook } from '../architecture/interactionHook/useCasesHook';
 import { ModuleEntity } from '../architecture/domain/entity';
 
 const CreateModule = () => {
     const cases = useCasesHook();
+    const [err, setErr] = useState({
+        status:false,
+        msg:""
+    });
     const dictionary = useAppSelector((state) => state.reducerLanguage).dictionary;
 
     const createModuleHandler = async (evt: FormEvent<HTMLFormElement>) => {
         evt.preventDefault();
         const dataFromForm = Object.fromEntries(new FormData(evt.target as HTMLFormElement));
         const bodyData: ModuleEntity = { name: String(dataFromForm['name']), description: String(dataFromForm['name']), moduleStartDate: String(dataFromForm['date']) };
-        const response = await cases.createModule(bodyData);
-        console.log(response);
+        await cases.createModule(bodyData).catch((err:Error) => {
+            setErr({
+                status:true,
+                msg:err.message
+            })
+        });
     };
-    console.log(process.env.NEXT_PUBLIC_BASE_PATH);
 
     return (
         <div className="w-10 flex flex-column justify-content-center align-items-center my-0 mx-auto gap-5">
             <form className="mt-6 flex flex-column w-8 gap-3" onSubmit={createModuleHandler}>
+                {err.status ? <p>{err.msg}</p>:<></>}
                 <h4>{dictionary.CreateModule}</h4>
                 <div className="w-full gap-3 flex flex-row gap  justify-start justify-items-center">
                     <InputPrimary size="large" title={dictionary.NameOfModule} placeHolder={dictionary.PholderNameModule} />
