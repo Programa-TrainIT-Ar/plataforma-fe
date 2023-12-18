3e2892eaa8fab6232d74be1a822ca6baa9f15ee2
Aramis jaime <you@example.com>
2023-12-18 17:06:37 -0300

    Delete innecesary comment

--- src/app/(main)/module/architecture/domain/entity.ts
+++ src/app/(main)/module/architecture/domain/entity.ts
@@ -1,18 +1,3 @@
-// export class ModuleEntity {
-//   id?: string;
-//   name: string;
-//   description: string;
-//   moduleStartDate: string;
-
-//   constructor({ id, name, description, moduleStartDate }: { id?: any, name: any, description: any, moduleStartDate: any }) {
-//     this.id = id
-//     this.name = name
-//     this.description = description
-//     this.moduleStartDate = moduleStartDate
-//   }
-// }
-
-
 export interface ModuleEntity {
   id?: string;
   name: string;
