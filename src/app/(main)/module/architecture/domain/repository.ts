3c55dc384ef8348733abf69cbd4e2a66b29fa7bd
Aramis jaime <you@example.com>
2023-12-21 20:05:56 -0300

    Add clean architecture structure to make work edit module funcionality

--- src/app/(main)/module/architecture/domain/repository.ts
+++ src/app/(main)/module/architecture/domain/repository.ts
@@ -3,6 +3,6 @@ import { ModuleEntity } from "./entity";
 
 export interface BaseRepositoryModule {
   create(bodyData: ModuleEntity): Promise<ResponseRequest>;
+  edit(bodyData: ModuleEntity): Promise<ResponseRequest>;
   search(bodyData: string): Promise<ResponseRequest>;
-
 }
--- src/app/(main)/module/architecture/infrastructure/repositoryImpl.ts
+++ src/app/(main)/module/architecture/infrastructure/repositoryImpl.ts
@@ -9,19 +9,19 @@ export class RepositoryModuleImpl implements BaseRepositoryModule {
     //! i need create endpoint backend to search by name
     try {
       const res = await (await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/modules`, {
-        method: "GET", 
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       })).json()
       //!this make a map to normalize the response with an adapter.
       const adaptedListModule = searchModulesAdapter(res)
-      
+
       const data: ResponseRequest = {
-        succes:true,
-        err:false,
-        message:"",
-        data:filterModulesByName(adaptedListModule,moduleName) //!this filter modules by name , i think we could make this in backend
+        succes: true,
+        err: false,
+        message: "",
+        data: filterModulesByName(adaptedListModule, moduleName) //!this filter modules by name , i think we could make this in backend
       }
       return data;
     } catch (error) {
@@ -33,21 +33,42 @@ export class RepositoryModuleImpl implements BaseRepositoryModule {
   async create(bodyData: ModuleEntity): Promise<ResponseRequest> {
     try {
       const res = await (await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/modules`, {
-        method: "POST", 
-        body: JSON.stringify(bodyData), 
+        method: "POST",
+        body: JSON.stringify(bodyData),
         headers: {
           "Content-Type": "application/json",
         },
       })).json()
       const data: ResponseRequest = {
-        succes:true,
-        err:false,
-        message:res,
-        data:null
+        succes: true,
+        err: false,
+        message: res,
+        data: null
       }
       return data;
     } catch (error) {
-      console.log("entre");
+      let err = error as Error
+      throw new Error(err.message)
+    }
+  }
+
+  async edit(bodyData: ModuleEntity): Promise<ResponseRequest> {
+    try {
+      const res = await (await fetch(`${process.env.NEXT_PUBLIC_BASE_PATH}/modules`, {
+        method: "PUT",
+        body: JSON.stringify(bodyData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })).json()
+      const data: ResponseRequest = {
+        succes: true,
+        err: false,
+        message: res,
+        data: null
+      }
+      return data;
+    } catch (error) {
       let err = error as Error
       throw new Error(err.message)
     }
