5efe74f78cfe389d393a66af906cdca089a13e9f
Aramis jaime <you@example.com>
2024-01-29 20:20:55 -0300

    add return type from useCasesHook

--- src/app/(main)/module/architecture/interactionHook/useCasesHook.tsx
+++ src/app/(main)/module/architecture/interactionHook/useCasesHook.tsx
@@ -2,9 +2,8 @@ import { useMemo, useRef } from 'react';
 import { UseCasesModule } from '../application/useCases';
 import { RepositoryModuleImpl } from '../infrastructure/repositoryImpl';
 
-export const useCasesHook = () => {
+export const useCasesHook = (): UseCasesModule => {
     const repositoryModuleImpl = new RepositoryModuleImpl();
     const cases = useRef(new UseCasesModule(repositoryModuleImpl));
-
     return cases.current;
 };
