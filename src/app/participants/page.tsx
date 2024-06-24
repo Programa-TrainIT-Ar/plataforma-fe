49b8b8b551d0eef2f9e1aa7b9a00f7ca11364d0e
Lucas Prat <lucasprat.cn@gmail.com>
2024-06-24 15:51:34 -0300

    add collapsed to fieldset and delete cosole.log

--- src/app/participants/page.tsx
+++ src/app/participants/page.tsx
@@ -4,12 +4,10 @@ import ParticipantsTable, { Participant, Cell } from './ParticipantsTable';
 export async function getParticipants() {
     try {
         const res = await fetch('https://api.example.com/partis');
-        console.log('res en get', res)
         if (!res.ok) {
             throw new Error('Failed to fetch data');
         }
         const response = await res.json()
-        console.log('response.json en', response)
         return response as Participant[];
     } catch (error) {
         console.error('error', error)
@@ -33,14 +31,11 @@ const Participants = async () => {
     const participants = await getParticipants();
     const cells = await getCells();
 
-    console.log('cells get', cells)
-    console.log('participants get', participants)
-
     return (
-        <div className="grid">
-            <h3>Participants</h3>
+        <>
+            <h3>Participantes</h3>
             <ParticipantsTable participants={participants} cells={cells} />
-        </div>
+        </>
     )
 };
 
