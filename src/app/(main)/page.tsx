7b7d78e33a837df4c34aa690d436ea90e33f49b5
Lucas Prat <lucasprat.cn@gmail.com>
2024-07-13 16:54:40 -0300

    create page /login, fix Cors error

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,20 +1,25 @@
 'use client'
 
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Link from 'next/link';
 import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { redirect } from 'next/navigation';
 
 const Dashboard = () => {
   const { user, error, isLoading } = useUser()
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  if (!user) return <Link href="/api/auth/login">Login</Link>;
-  return (<div className="flex flex-column">
-    DASHBOARD
-    <div>Hello {user.nickname}</div>
-    <Link href="/api/auth/logout">Logout</Link>
-  </div>);
+  if (!user) {redirect('/login')}
+
+  return (
+    <div className="flex flex-column">
+      <h3>DASHBOARD</h3>
+      <br></br>
+      <div>{JSON.stringify(user)}</div>
+      <br></br>
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  );
 };
 
 export default Dashboard;
