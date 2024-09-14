02b7e988ee2cab5ede79eea5041e63bc05ec830a
Leonel A.T. Goncalves <liongoncalves@gmail.com>
2024-09-14 19:32:22 +0200

    Dep update

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,40 +5,18 @@ import Providers from '@/services/Providers';
 import Layout from '@/layout/layout';
 
 export default async function AppLayout({ children }: AppLayoutProps) {
-  // const token = await getAccessToken().catch(redirect('/login'))
-  // const accessToken = token?.accessToken
-  // console.log('token en layout dashboard', accessToken)
-
-  // return (
-  //   <html lang="en" suppressHydrationWarning>
-  //     <head>
-  //       <link id="theme-css" href={`/themes/bootstrap4-dark-purple/theme.css`} rel="stylesheet"></link>
-  //     </head>
-  //     <body>
-  //         {/* <Providers accessToken={token?.accessToken}> */}
-  //           {children}
-  //         {/* </Providers> */}
-  //     </body>
-  //   </html>
-  // );
-
   try {
     const token = await getAccessToken();
     console.log('token en layout dashboard', token);
     return (
-      <html lang="en" suppressHydrationWarning>
-        <head>
-          <link id="theme-css" href={`/themes/bootstrap4-dark-purple/theme.css`} rel="stylesheet"></link>
-        </head>
-        <body>
-          <Providers accessToken={token?.accessToken}>
-            <Layout>{children}</Layout>
-          </Providers>
-        </body>
-      </html>
+      <Providers accessToken={token?.accessToken}>
+        <Layout>{children}</Layout>
+      </Providers>
+
     );
   } catch (error) {
     console.error(error);
     redirect('/login');
-  };
+  }
+
 };
