136a0f47da0c9003240652eb9d522e993d415759
lucia aguirre <guerrolucia@gmail.com>
2024-09-25 10:10:13 +0200

    feat: adds table

---
+++ src/app/editions/EditionsTable.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const EditionsTable = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Edicion</th>
+                    <th>Nombre</th>
+                    <th>Proyecto</th>
+                </tr>
+            </thead>
+            <tbody>{/* Añadir filas aquí */}</tbody>
+        </table>
+    );
+};
+
+export default EditionsTable;
